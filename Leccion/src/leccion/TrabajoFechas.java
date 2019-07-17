18a4a64614a43c8cf261087da53d0a0562fa27c9
LENOVO <LENOVO@LAPTOP-39EC5ES9>
2019-07-16 19:29:19 -0500

    Sincronizando el proyecto

--- Leccion/src/leccion/TrabajoFechas.java
+++ Leccion/src/leccion/TrabajoFechas.java
@@ -13,13 +13,17 @@ import java.util.Date;
  */
 public class TrabajoFechas {
     public static void main(String[] args) {
-        Date fecha = new Date();
+       /* Date fecha = new Date();
         System.out.println(fecha);
         System.out.println("Year:"+ (fecha.getYear()+1900));
         System.out.println("Month:"+fecha.getMonth());
         System.out.println("Day:"+fecha.getDay());
-        System.out.println("Hour:"+fecha.getHours());
+        System.out.println("Hour:"+fecha.getHours());*/
        
+       Date fecha = new Date(101,6,18);
+       Date fechaActual = new Date();
+        System.out.println(fecha.after(fechaActual));
+        System.out.println(fecha.before(fechaActual));
     }
     
 }
