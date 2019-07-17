b9a636b9d1ff8f7747f7db07dee12f0ce55e6697
LENOVO <LENOVO@LAPTOP-39EC5ES9>
2019-07-16 19:06:11 -0500

    Sincronizando el proyecto

---
+++ Leccion/src/leccion/TrabajoFechas.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leccion;
+
+import java.util.Date;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class TrabajoFechas {
+    public static void main(String[] args) {
+        Date fecha = new Date();
+        System.out.println(fecha);
+    }
+    
+}
