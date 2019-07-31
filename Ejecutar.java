2bd6f56968cc1b2e414481612397b6f90699e4db
Michelle <51102312+Michelle-Zhanay-1994@users.noreply.github.com>
2019-07-30 23:23:42 -0500

    Add files via upload

--- Ejecutar.java
+++ Ejecutar.java
@@ -1,48 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TRABAJO_BIMESTRAL;
 
-package Calculo;
-
-import static Calculo.Calcular.grados;
 import java.util.Scanner;
 
-
-public class Ejecutar {
-
-    private static double valor;
-    private static double grados;
+/**
+ *
+ * @author LENOVO
+ */
+public class Ejecutar extends Fecha {
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        System.out.println("Elige la funcion trigonometrica: \n 1 para Seno \n 2 para Coseno \n 3 para Tangente");
-        int res = entrada.nextInt();
-        switch (res) {
-            case 1:
-               CalculoSeno objeto = new CalculoSeno();
-                System.out.println("Ingrese el valor");
-                double grado = entrada.nextDouble();
-                double valor = entrada.nextDouble();
-                System.out.println("El grado es "+objeto.CalculoGrados(grado));
-                System.out.println("El valor de seno es "+objeto.CalculoSeno(valor));
-                break;
-            case 2:
-               CalculoCoseno objeto1 = new CalculoCoseno();
-                System.out.println("Ingrese el valor");
-                double grado1 = entrada.nextDouble();
-                double valor1 = entrada.nextDouble();
-                System.out.println("El grado es "+objeto1.CalculoGrado1(grado1));
-                System.out.println("El valor de seno es "+objeto1.CalculoCoseno(valor1));
-               break;
-            case 3:
-                CalculoTangente objeto2 = new CalculoTangente();
-                System.out.println("Ingrese el valor");
-                double grado2 = entrada.nextDouble();
-                double valor2 = entrada.nextDouble();
-                System.out.println("El grado es "+objeto2.CalculoGrado2(grado2));
-                System.out.println("El valor de seno es "+objeto2.CalculoTangente(valor2));
-                break;
-        }
+        Scanner objeto = new Scanner(System.in);
+        
     }
-
-   
-
- 
-}
     
+    public Ejecutar(int dia, int mes, int año, int fechaactual, int fecha, String esBisiesto) {
+        super(dia, mes, año, fechaactual, fecha, esBisiesto);
+    }
+    
+}
